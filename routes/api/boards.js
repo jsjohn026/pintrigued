225b6bbe5aff2903c7f82ec49b6157877656bc6d
Oliver Almalel <oliver.almalel@gmail.com>
2019-06-04 15:22:10 -0700

    first commit

--- routes/api/boards.js
+++ routes/api/boards.js
@@ -1,6 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the boards route" }));
+
+
+// index
+// router.get("/")
+
+// index by user
+// router.get("/user/:user_id")
+
+// show by id
+// router.get("/:id")
+
+// create
+// router.post("/")
+
+// update
+// router.patch("/:userId")
+
+// delete
+// router.delete("/:id")
+
+
+
+
 
 module.exports = router;
