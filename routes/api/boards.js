f725c1fed65bbad69458ddd64c37658ce8a55aa1
Jasmine <jsjohn026@gmail.com>
2019-06-04 18:59:57 -0500

    add update and delete routes

--- routes/api/boards.js
+++ routes/api/boards.js
@@ -1,6 +1,55 @@
 const express = require("express");
 const router = express.Router();
+const passport = require('passport');
+const Board = require('../../models/Board');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the boards route" }));
 
 module.exports = router;
+
+// index
+// router.get("/")
+
+// index by user
+// router.get("/user/:user_id")
+
+// show by id
+// router.get("/:id")
+
+// create
+// router.post("/")
+
+
+// update
+router.patch('/:boardId', passport.authenticate('jwt', {session: false }), (req, res) => {
+  Board.findOne({
+    _id: req.params.boardId, 
+    userId: req.user.id
+  })
+  .then(board => {
+    if (board) {
+      board.title = req.body.title;
+      board.description = req.body.description;
+      board.save()
+      .then(board => res.json(board))
+    } else {
+      return res.status(404).json("Board not found");
+    }
+  })
+})
+
+// delete
+router.delete('/:boardId', passport.authenticate('jwt', {session: false }), (req, res) => {
+  Board.findOne({
+    _id: req.params.boardId, 
+    userId: req.user.id
+  })
+  .then(board => {
+    if (board) {
+      board.remove()
+      .then( () => res.json({message: "success"}))
+    } else {
+      return res.status(404).json("Board not found");
+    }
+  })
+})
