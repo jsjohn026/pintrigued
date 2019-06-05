ff0d9f442549fc362681bfe3b490a3679756fe75
Oliver Almalel <oliver.almalel@gmail.com>
2019-06-04 20:48:08 -0700

    merge boards branch into sub-branch

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-<<<<<<< HEAD
 import { Route, Switch } from 'react-router-dom';
-=======
-import { Switch } from 'react-router-dom';
-import NavBarContainer from './nav/navbar_container';
-import CreateBoardForm from './boards/create_board_form';
-import './root.css';
-
->>>>>>> boards
 import MainPage from './main/main_page';
 import './root.css'
 import NavBar from './nav/navbar_container';
@@ -17,7 +9,6 @@ import LoginForm from './session/login_form_container';
 import SignupForm from './session/signup_form_container';
 
 const App = () => (
-<<<<<<< HEAD
   <div className="root-container">
     <div className="root">
       <NavBar />
@@ -26,16 +17,6 @@ const App = () => (
         <AuthRoute exact path="/login" component={ LoginForm } />
         <Route path="/user/:userId/boards" component={ BoardsIndex } />
         <AuthRoute exact path="/" component={ MainPage } />
-=======
-  <div className='root-container'>
-    <div className='root'>
-      <NavBarContainer />
-      <Switch>
-        <AuthRoute exact path='/signup' component={SignupFormContainer} />
-        <AuthRoute exact path='/login' component={LoginFormContainer} />
-        <AuthRoute exact path='/' component={MainPage} />
-        <ProtectedRoute exact path='/boards' component={CreateBoardForm} />
->>>>>>> boards
       </Switch>
     </div>
   </div>
