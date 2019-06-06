b504fc5a2a0620dde502a55265dc260299d30bef
Oliver Almalel <oliver.almalel@gmail.com>
2019-06-06 08:14:32 -0700

    ignore keys_dev, remove extra server port load

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI
 const users = require('./routes/api/users');
