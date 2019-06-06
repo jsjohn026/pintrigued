6673b3149f09ac4e8e5c34c5b31e60dbe369051f
Oliver Almalel <oliver.almalel@gmail.com>
2019-06-05 19:08:46 -0700

    | update config keys keys_dev keys_prod | updated .gitignore | fix merge conflict |

--- frontend/src/components/boards/boards_index_item.jsx
+++ frontend/src/components/boards/boards_index_item.jsx
@@ -11,22 +11,6 @@ class BoardIndexItem extends React.Component {
     const { board } = this.props
     const { title } = board
     return (
-<<<<<<< HEAD
-      <div className="boards-index-item-container">
-        {this.state.showEdit ?  <EditBoardForm board={board} closeModal={() => this.setState({ showEdit: false})} /> : null }
-        <div className="boards-index-item-bg">
-          <div className="pins-container"> {/* move className to pins coponent when ready */}
-            {/* future Pins */}
-          </div>
-
-          <div className="boards-index-item-tail-container">
-            <div className="boards-index-item-tail">
-              
-              <div className="boards-index-item-title-container">
-                <div onClick={() => this.setState({ showEdit: true})}>Edit</div>
-                <div className="boards-index-item-title">
-                  { title }
-=======
       <div className="boards-index-item-holder">
         <div className="boards-index-item-container">
           <div className="boards-index-item-bg">
@@ -41,7 +25,6 @@ class BoardIndexItem extends React.Component {
                   <div className="boards-index-item-title">
                     { title }
                   </div>
->>>>>>> 31b617de26d5800b0e47f24d1183a8f2741c03e4
                 </div>
 
                 <div className="board-index-item-pins">
