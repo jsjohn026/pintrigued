f6f025dfebeb363e486a1ad4b4a5e6bcb1c202a9
Oliver Almalel <oliver.almalel@gmail.com>
2019-06-07 15:38:51 -0700

    cleanup/refactor upload.js

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -1,22 +1,30 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const passport = require('passport');
-const bcrypt = require('bcryptjs');
 const Pin = require('../../models/Pin');
 const multer = require('multer');
 var AWS = require('aws-sdk');
 const validatePinInput = require('../../validation/pins');
 
-
 var storage = multer.memoryStorage();
 var upload = multer({ storage: storage });
 
+const createPin = (body, res) => {
+  const { title, description, imageUrl, linkUrl } = body
+  newPin = new Pin({
+    title,
+    description,
+    imageUrl,
+    linkUrl,
+  });
+  newPin.save().then(pin => res.json(pin));
+}
+
 router.post('/', upload.single('file'), (req, res) => {
-  
-  if (req.file) {
-    const { file } = req
+  const { body, file } = req
+
+  if (file) {
+
     const s3FileURL = process.env.AWS_UPLOADED_FILE_URL;
     const randomURL =
       Math.random().toString(36).substring(2, 15) +
@@ -34,31 +42,20 @@ router.post('/', upload.single('file'), (req, res) => {
       ACL: 'public-read'
     };
 
-    s3bucket.upload(params, (err, data) => {
-    if (err) {
-      res.status(500).json({ error: true, Message: err });
-    } else {
-      const newPin = new Pin({
-        title: req.body.title,
-        description: req.body.description,
-        imageUrl: s3FileURL + randomURL,
-        linkUrl: req.body.linkUrl,
-        s3_key: params.Key
-      });
-      newPin.save().then(pin => res.json(pin));
-    }});
+    s3bucket.upload(params, err => {
+      if (err) {
+        res.status(500).json({ error: true, Message: err });
+      } else {
+        body.imageUrl = s3FileURL + randomURL
+        createPin(body, res)
+      }
+    });
+
   } else {
 
-    const { errors, isValid } = validatePinInput(req.body);
+    const { errors, isValid } = validatePinInput(body);
     if (!isValid) return res.status(400).json(errors)
-
-    const newPin = new Pin({
-      title: req.body.title,
-      description: req.body.description,
-      imageUrl: req.body.imageUrl,
-      linkUrl: req.body.linkUrl,
-    });
-    newPin.save().then(pin => res.json(pin));
+    createPin(body, res)
   }
 });
 
