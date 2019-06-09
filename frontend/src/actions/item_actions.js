4f8b6cae139beec8062987d85b98ff2b0c8a451a
John-Michael Riley <johnmichael.w.riley@gmail.com>
2019-06-08 20:26:37 -0400

    commit item actions and item util to enable pull

--- frontend/src/actions/item_actions.js
+++ frontend/src/actions/item_actions.js
@@ -1,44 +1,43 @@
-import * as APIUtil from "../util/items_api_util";
+import * as APIUtil from '../util/items_api_util';
 
-export const RECEIVE_ITEM_ERRORS = "RECEIVE_ITEM_ERRORS";
-export const RECEIVE_BOARD_ITEMS = "RECEIVE_BOARD_ITEMS ";
-export const RECEIVE_USER_ITEMS = "RECEIVE_USER_ITEMS";
-export const RECEIVE_ITEM = "RECEIVE_ITEM";
-export const REMOVE_ITEM = "REMOVE_ITEM";
+export const RECEIVE_ITEM_ERRORS = 'RECEIVE_ITEM_ERRORS';
+export const RECEIVE_BOARD_ITEMS = 'RECEIVE_BOARD_ITEMS ';
+export const RECEIVE_USER_ITEMS = 'RECEIVE_USER_ITEMS';
+export const RECEIVE_ITEM = 'RECEIVE_ITEM';
+export const REMOVE_ITEM = 'REMOVE_ITEM';
 
 const receiveItemErrors = errs => ({
-  type: RECEIVE_ITEM_ERRORS, 
+  type: RECEIVE_ITEM_ERRORS,
   errs
 });
 
 const receiveBoardItems = items => ({
-  type: RECEIVE_BOARD_ITEMS, 
+  type: RECEIVE_BOARD_ITEMS,
   items
 });
 
 const receiveUserItems = items => ({
-  type: RECEIVE_USER_ITEMS, 
+  type: RECEIVE_USER_ITEMS,
   items
 });
 
 const receiveItem = item => ({
-  type: RECEIVE_ITEM, 
+  type: RECEIVE_ITEM,
   item
 });
 
 const removeItem = data => ({
-  type: REMOVE_ITEM, 
+  type: REMOVE_ITEM,
   itemId: data.itemId
 });
 
 export const updateItem = item => dispatch => {
   return APIUtil.updateItem(item)
-  .then(res => dispatch(receiveItem(res.data)))
-  .catch(errs => dispatch(receiveItemErrors(errs.response.data))
-  )
-}
+    .then(res => dispatch(receiveItem(res.data)))
+    .catch(errs => dispatch(receiveItemErrors(errs.response.data)));
+};
 
 export const deleteItem = itemId => dispatch =>
   APIUtil.deleteItem(itemId)
-  .then(res => dispatch(removeItem(res.data)))
-  .catch(err => console.log(err.response.data));
+    .then(res => dispatch(removeItem(res.data)))
+    .catch(err => console.log(err.response.data));
--- frontend/src/util/items_api_util.js
+++ frontend/src/util/items_api_util.js
@@ -5,15 +5,15 @@ import axios from 'axios';
 // };
 
 export const getUserItems = userId => {
-  return axios.get(`/api/items/users/${ userId }`);
+  return axios.get(`/api/items/users/${userId}`);
 };
 
 export const getBoardItems = boardId => {
-  return axios.get(`/api/items/boards/${ boardId }`);
+  return axios.get(`/api/items/boards/${boardId}`);
 };
 
 export const getItem = itemId => {
-  return axios.get(`/api/items/${ itemId }`);
+  return axios.get(`/api/items/${itemId}`);
 };
 
 export const createItem = item => {
@@ -21,9 +21,9 @@ export const createItem = item => {
 };
 
 export const updateItem = item => {
-  return axios.patch(`/api/items/${ item._id }`, item);
+  return axios.patch(`/api/items/${item._id}`, item);
 };
 
 export const deleteItem = itemId => {
-  return axios.delete(`/api/items/${ itemId }`);
+  return axios.delete(`/api/items/${itemId}`);
 };
