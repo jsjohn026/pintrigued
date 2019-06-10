071822c7daf2ed3b47a73fb232fce0d54291b478
Oliver Almalel <oliver.almalel@gmail.com>
2019-06-10 14:19:23 -0700

    update main page jsx and remove stacks, stacks_container

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 import PinsIndex from '../pins/pins_index_container'
-import Stacks from '../pins/stacks_container'
 
 export class MainPage extends Component {
   render() {
     return (
       <div>
         <PinsIndex />
-        {/* <Stacks className="bricks-container-app" /> */}
         <h1>A Pinterest Clone</h1>
         <footer>
           Copyright &copy; 2019 Pintrigue
